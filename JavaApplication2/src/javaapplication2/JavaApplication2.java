9b619a26fc06e546166a857f02ee3207897af8c5
mxfrd <feraud.max@gmail.com>
2014-11-27 18:24:15 +0100

    test commit nebean

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapptest;
+package javaapplication2;
 
 /**
  *
  * @author Max
  */
-public class JavaAppTest {
+public class JavaApplication2 {
 
     /**
      * @param args the command line arguments
