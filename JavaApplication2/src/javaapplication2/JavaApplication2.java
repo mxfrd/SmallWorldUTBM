98a154b149e417737f78b9ff29a572baf792c746
mxfrd <feraud.max@gmail.com>
2014-11-28 10:19:57 +0100

    test commit nebean

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -15,7 +15,9 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        System.out.println("Test Commit / Push ");
     }
     
 }
